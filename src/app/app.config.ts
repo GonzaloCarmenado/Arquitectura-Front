457c57a7873826ff67ca162615958b7f54f25d9f
Gonzalo Carmenado <gonzalocarmenado@gmail.com>
2025-05-18 19:24:59 +0200

    Add: new intterceptor added to app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,18 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { GeneralHttpInterceptorService } from 'general-http-interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),
     provideAnimations(),
     importProvidersFrom(
       BrowserAnimationsModule,
-   
-    )
+    ),
+     provideHttpClient(
+      withInterceptors([GeneralHttpInterceptorService]),
+    ),
   ]
 };
